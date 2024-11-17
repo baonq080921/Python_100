eae3ea7080d723f7a0d9491ea9f970b8108d8cf7
bao <bao@gmail.com>
2024-11-17 18:11:20 +0700

    second commit

--- src/Day7/_hang_man.py
+++ src/Day7/_hang_man.py
@@ -1,41 +1,55 @@
 import random
+import _hang_man_art as  art
+print(art.logo)
+def main():
+    lives = 6
+    correct_letter = []
+    i =0
+    game_end = False
+    keyword = ['apple', 'banana', 'cherry','grape','orange','pear','watermelon']
+    # random_int =random.randint(1,5)
+    random_choice = random.choice(keyword)
+    print(random_choice)
+    n = len(random_choice)
+    print(n)
+    word_guess =""
+    for i  in range (1,n+1):
+        word_guess+="_"
+    print("Word to guess:"+word_guess)
+    print('\n')
+    user_input = input("Press any key to continue")
+    player_won = False
+    while(not game_end):
+        display = ""
+        user_input = input("Guess a letter:")
 
-print("HANG MAN:")
-print("\n")
+        for letter in random_choice:
+            if letter == user_input:
+                display += letter
+                correct_letter.append(user_input)
+            elif letter in correct_letter:
+                display += letter
 
-keyword = 'apple'
-# random_int =random.randint(1,5)
-random_int = len(keyword)
-word_guess =""
-for i  in range (1,random_int+1):
-    word_guess+="_"
-print("Word to guess:"+word_guess)
+            elif letter != user_input:
+                display += "_"
+        print(display)
 
-user_input = input("Press any key to continue")
-player_won = False
+        if user_input in correct_letter:
+            print(f"You've already guessed {letter}")
+            print(art.stages[lives])
+        elif user_input not in correct_letter:
+            lives -= 1
+            print(f'You guess {user_input} wrong letter, Please try again')
+            print(art.stages[lives])
+            if (lives == 0):
+                game_end = True
+                print('You lose')
 
-count =0
-correct_letter =[]
-while(player_won == False):
-    display = ""
-    user_input = input("Guess a letter")
-    for letter in keyword:
-        if letter == user_input:
-            display += letter
-            correct_letter.append(user_input)
-        elif letter in correct_letter:
-            display += letter
-
-        elif letter != user_input:
-            display += "_"
-            count = 4
-
-    print(display)
-
-    if "_" not in display:
-        player_won = True
-        print("Congratulations! You won")
-
+        if "_" not in display:
+            game_end=True
+            print("Congratulations! You won")
+if __name__ == "__main__":
+    main()
 
 
 
